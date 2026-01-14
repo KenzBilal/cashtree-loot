c34cd3fc65345410d128bc5acd15a9a9e36fbbcd
Kenz Bilal <kenzbilal12@gmail.com>
2026-01-14 11:53:27 +0530

    Fix config syntax

--- cashtree-loot/next.config.mjs
+++ cashtree-loot/next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // <--- This line fixes the error!
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true,
+  },
 };
 
- module.exports = nextConfig;
+export default nextConfig;
