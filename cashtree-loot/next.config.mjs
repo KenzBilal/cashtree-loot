dd4bd5a281ef978e1c2559f9e1176c0ad7affcf0
Kenz Bilal <kenzbilal12@gmail.com>
2026-01-14 11:23:11 +0530

    Fix cloudflare deployment

--- cashtree-loot/next.config.mjs
+++ cashtree-loot/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',  // <--- This line fixes the error!
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+ module.exports = nextConfig;
