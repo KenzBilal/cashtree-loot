80c6287908b4109aed11db8746e56951b8228773
Kenz Bilal <kenzbilal12@gmail.com>
2026-01-23 12:16:59 +0530

    Final

---
+++ cashtree-loot/public/dashboard/config.js
@@ -0,0 +1,4 @@
+window.env = {
+    SUPABASE_URL: "https://qzjvratinjirrcmgzjlx.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6anZyYXRpbmppcnJjbWd6amx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyMzAxMDksImV4cCI6MjA4MzgwNjEwOX0.W01Pmbokf20stTgkUsmI3TZahXYK4PPbU0v_2Ziy9YA"
+};
