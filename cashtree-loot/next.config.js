5b60ee8b7d34ce4b9bfb535c50cb4b4706d604bc
Kenz Bilal <kenzbilal12@gmail.com>
2026-01-28 21:23:08 +0530

    Fix layout CSS path

--- cashtree-loot/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export',  <--- DELETE OR COMMENT THIS OUT
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
